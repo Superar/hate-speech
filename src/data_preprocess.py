66bb3af3e01968c1ba7eb1b8cd95f6962cda8e75
Ana Caroline <carolmedeirosbrito@gmail.com>
2019-04-15 20:17:31 -0300

    features selected by correlation value

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,39 +1,41 @@
 #! /usr/bin/env python3
 import os
-import pandas as pd
 import numpy as np
+import pandas as pd
+from scipy.stats import pearsonr
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 
-def read_data(path):
-    
+def read_data(metadata,path):
     filenames = os.listdir(path)
-    file_ids = [os.path.splitext(f)[0] for f in filenames]
-
-    corpus = [open(path+f).read() for f in filenames]
-    vectorizer = CountVectorizer(stop_words=stopwords.words('english')) # n_features=2**12
-    corpus = vectorizer.fit_transform(corpus)
-
-    corpus_pd = pd.DataFrame(corpus.toarray(),columns=vectorizer.vocabulary_)
     
-    return file_ids,corpus_pd
-
-def select_by_corr(corpus,file_ids):
+    file_ids = [os.path.splitext(f)[0] for f in filenames]
     labels = metadata[metadata['file_id'].isin(file_ids)].label
-    labels = pd.get_dummies(labels)
-    corr = corpus.corrwith(labels['hate'])
-    # nans = corr[corr.isna()].index
-    # for word in nans:
-    #     print(word,sum(corpus[word]))
-    # corr = corr.fillna(value=0)
-    not_nans = corr[corr.notna()].index
-    for word in not_nans:
-        print(word,sum(corpus[word]))
-    return corr
+    labels_pd = pd.get_dummies(labels)['hate']
+    
+    corpus = [open(path+f).read() for f in filenames]
+    vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
+    corpus = vectorizer.fit_transform(corpus)
+    corpus_pd = pd.DataFrame(corpus.toarray(),columns=vectorizer.get_feature_names())
+    
+    return corpus_pd,labels_pd
+
+def select_by_corr(corpus,labels,top_n=1000):
+    corrs = []
+    for col in corpus:
+        col = corpus[col].values
+        corr, _ = pearsonr(col, labels)
+        corrs.append(corr)
+
+    cols = corpus.columns
+    sorted_idxs = np.argsort(np.abs(corrs))[-top_n:]
+    top_sorted = cols[sorted_idxs]
+
+    return top_sorted
     
 metadata = pd.read_csv('hate-speech-dataset/annotations_metadata.csv')
-file_ids,corpus = read_data('hate-speech-dataset/sampled_train/')
+corpus,labels = read_data(metadata,'hate-speech-dataset/sampled_train/')
 
-
-
-select_by_corr(corpus,file_ids)
+selected_feats = select_by_corr(corpus,labels)
+corpus = corpus[selected_feats]
+corpus.to_csv('processed_train.csv')
